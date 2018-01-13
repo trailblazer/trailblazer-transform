69bb53fba4c776506baaecb609dde613b4bbd497
Nick Sutterer <apotonick@gmail.com>
2018-01-13 14:17:28 +0100

    make PriceFloat a separate activity. this will allow reuse for my example.

--- test/transform_test.rb
+++ test/transform_test.rb
@@ -8,12 +8,10 @@ class TransformTest < Minitest::Spec
 
 require "trailblazer/operation"
 
-class ExpenseUnitPrice < Trailblazer::Operation
-  step :parse # sucess: fragment found
-  fail :error_required, fail_fast: true # implies it wasn't sent! here, we could default
-
+class PriceFloat < Trailblazer::Operation
+  step :filled?
   step :coerce_string # success: is string, fail: is nil
-  fail :error_string, fail_fast: true
+  fail :error_string, fail_fast: true # FragmentNotFound/FragmentBlank
   # step :empty?
   step :trim
   step :my_format
@@ -21,33 +19,18 @@ class ExpenseUnitPrice < Trailblazer::Operation
   step :coerce_float
   step :float_to_int # * 100
 
-  step :set
-
-  # fail :parse_items_zero
-
-
-  # We could use Representable here.
-  def parse(ctx, **)
-    return unless ctx.key?(:unit_price)
-    ctx[:value] = ctx[:unit_price]
-    true
-  end
-
-  # I could be an End event, no step
-  def error_required(ctx, **)
-    ctx[:error] = "Fragment :unit_price not found"
-    false
-  end
   def error_string(ctx, value:, **)
-    ctx[:error] = "#{value.inspect} is no string"
+    ctx[:error] = "#{value.inspect} is blank string"
   end
   def error_format(ctx, value:, **)
     ctx[:error] = "#{value.inspect} is wrong format"
   end
 
+  def filled?(ctx, value:, **)
+    ! value.nil?
+  end
   # TODO: use dry-types here
   def coerce_string(ctx, value:, **)
-    return if value.nil?
     value.to_s
   end
 
@@ -66,11 +49,73 @@ class ExpenseUnitPrice < Trailblazer::Operation
   def float_to_int(ctx, value:, **)
     ctx[:value] = (value * 100).to_i
   end
+end
+
+class ExpenseUnitPrice < Trailblazer::Operation
+  step :parse # sucess: fragment found
+  fail :error_required, fail_fast: true # implies it wasn't sent! here, we could default
+
+
+  step :set
+
+  # fail :parse_items_zero
+
+
+  # We could use Representable here.
+  def parse(ctx, **)
+    return unless ctx.key?(:unit_price)
+    ctx[:value] = ctx[:unit_price]
+    true
+  end
+
+  # I could be an End event, no step
+  def error_required(ctx, **)
+    ctx[:error] = "Fragment :unit_price not found"
+    false
+  end
+  def error_format(ctx, value:, **)
+    ctx[:error] = "#{value.inspect} is wrong format"
+  end
+
 
   def set(ctx, value:, model:, **)
     model.unit_price = value
   end
 end
+
+  # PriceFloat
+  #  ends:
+  #   => FragmentNotFound/FragmentBlank
+  #   => End.success
+  #   => End.failure, invalid
+  #
+  #  interface
+  #   ctx[:error]
+  #   ctx[:value]
+  describe "PriceFloat" do
+    let(:activity) { PriceFloat.decompose.first }
+
+    it "fragment nil" do
+      signal, (ctx, _) = activity.( [ { value: nil }, {} ], exec_context: PriceFloat.new )
+
+      signal.class.inspect.must_equal %{Trailblazer::Operation::Railway::End::FailFast}
+      ctx[:error].must_equal %{nil is blank string}
+    end
+
+    it "wrong format" do
+      signal, (ctx, _) = activity.( [ { value: " bla " }, {} ], exec_context: PriceFloat.new )
+
+      signal.class.inspect.must_equal %{Trailblazer::Operation::Railway::End::Failure}
+      ctx[:error].must_equal %{"bla" is wrong format}
+    end
+
+    it "correct format" do
+      signal, (ctx, _) = activity.( [ { value: "9.8" }, {} ], exec_context: PriceFloat.new )
+
+      signal.class.inspect.must_equal %{Trailblazer::Operation::Railway::End::Success}
+      ctx[:value].must_equal 980
+    end
+  end
 
   let(:activity) { ExpenseUnitPrice.decompose.first }
 
@@ -85,7 +130,7 @@ end
     signal, (ctx, _) = activity.( [ { unit_price: nil }, {} ], exec_context: ExpenseUnitPrice.new )
 
     signal.class.inspect.must_equal %{Trailblazer::Operation::Railway::End::FailFast}
-    ctx[:error].must_equal %{nil is no string}
+    ctx[:error].must_equal %{nil is blank string}
   end
 
   it "wrong format" do
@@ -105,6 +150,10 @@ end
 =begin
 
 # Reform was too declarative. No dynamic checks, etc, and too much implicit behavior (like parsing/populator)
+# Reform: mix of representable and its own chains, hard to extend. Let's keep the "steps" but rewire them.
+# The problem: a coercion _is_ a validation, but without an error msg etc, so why not simply chain it using
+# the Activity mechanics that we already have?
+# Much much more explicit than Reform
 
   # In the UI, we want the "Reform style" where an object graph represents the form UI
   class Transformer::UI # this is the document coming in
