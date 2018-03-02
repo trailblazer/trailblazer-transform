28ca9da4aef621dc281e7b802ca539c6f1b37e55
Nick Sutterer <apotonick@gmail.com>
2018-03-02 21:05:03 +0100

    Schema now allows to setup minimal "Reform"-style processing instructions!

--- lib/trailblazer/transform/schema.rb
+++ lib/trailblazer/transform/schema.rb
@@ -1,41 +1,51 @@
-module Trailblazer::Transform
-  module Schema
-    # Create an activity to read, process and write a scalar property.
-    # The activity looks as follows.
-    #
-    # [Read] -> [your process] -> [Write] -> End(:success)
-    #   |             |--------------------> End(:failure)
-    #   |
-    #   -----------------------------------> End(:required)
-    #
-    # This activity is then added via `task Subprocess(<activity>)`.
-    #
-    # If the placement/wiring of automatically provided {activity} doesn't suit you,
-    # use {:override}. # TODO, MORE DOCS.
-    def property(name, processor:, **options)
-      flow = Module.new do
-        extend Trailblazer::Activity::Railway(name: name)
+module Trailblazer
+  module Transform
+    module Schema
+      module_function
 
-        step Parse::Hash::Step::Read.new(name: name), Output(:failure) => End(:required)
-        step Subprocess(processor)#, Output(:fail_fast) => "required"
-        pass Trailblazer::Transform::Process::Write.new(writer: "#{name}=")
+      # Create an activity to read, process and write a scalar property.
+      # Write it to the passed {activity}.
+      #
+      # The activity looks as follows.
+      #
+      # [Read] -> [your process] -> [Write] -> End(:success)
+      #   |             |--------------------> End(:failure)
+      #   |
+      #   -----------------------------------> End(:required)
+      #
+      # This activity is then added via `task Subprocess(<activity>)`.
+      #
+      # If the placement/wiring of automatically provided {activity} doesn't suit you,
+      # use {:override}. # TODO, MORE DOCS.
+      def property(activity, name, processor:, **options)
+        flow = Module.new do
+          extend Activity::Railway(name: name)
+
+          step Parse::Hash::Step::Read.new(name: name), Output(:failure) => End(:required)
+          step Subprocess(processor)#, Output(:fail_fast) => "required"
+          pass Transform::Process::Write.new(writer: "#{name}=")
+        end
+
+        insert(activity, name, processor: flow, **options)
       end
 
-      insert(name, processor: flow, **options)
-    end
+      def collection(activity, name, item_processor:, **options)
+        processor = Transform::Process::Collection.new(activity: item_processor)
 
-    def collection(name, item_processor:, **options)
-      processor = Trailblazer::Transform::Process::Collection.new(activity: item_processor)
+        property(activity, name, processor: processor, **options)
+      end
 
-      property(name, processor: processor, **options)
-    end
+      # private
 
-    private
+      def insert(activity, name, processor:, override: nil, **options)
+        return instance_exec(processor, &override) if override
 
-    def insert(name, processor:, override: nil, **options)
-      return instance_exec(processor, &override) if override
+        connections = {} # with empty connections, processor.success goes to the next property, everything else Ends.
+        connections = { Activity::DSL::Helper::Output(:failure)  => Activity::DSL::Helper::Track(:success) ,
+                        Activity::DSL::Helper::Output(:required) => Activity::DSL::Helper::Track(:success) } # "Reform-style": parse and validate all
 
-      task Subprocess( processor )
+        activity.task Activity::DSL::Helper::Subprocess( processor ), connections
+      end
     end
   end
 end
--- test/flow_test.rb
+++ test/flow_test.rb
@@ -2,10 +2,24 @@ require "test_helper"
 
 module Trailblazer::Transform
   class Flow
-    extend Trailblazer::Activity::Path()
-    extend Schema
+    class << self
+      attr_accessor :activity
 
+      def property(*args, &block)
+        Schema.property(activity, *args, &block)
+      end
+    end
 
+    def self.inherited(subclass)
+      activity = Module.new do # todo: MOVE TO SCHEMA
+        extend Trailblazer::Activity::Path()
+
+        _end task: End(:required), group: :end, magnetic_to: [:required]
+        _end task: End(:failure), group: :end, magnetic_to: [:failure] # TODO: rename to :invalid?
+      end
+
+      subclass.activity = activity # fixme. doesn't inherit existing shit.
+    end
   end
 end
 
@@ -14,7 +28,9 @@ class FlowTest < Minitest::Spec
     extend Trailblazer::Activity::Path()
 
     pass ->(ctx, value:, **) { ctx[:value] = "Amount: #{value}" }
+
     _end task: End(:required)#, magnetic_to: [:required]
+    _end task: End(:failure)#, magnetic_to: [:required]
   end
 
   module InvoiceDate
@@ -29,4 +45,6 @@ class FlowTest < Minitest::Spec
     property :invoice_date, processor: InvoiceDate
     # collection :items
   end
+
+  pp Expense.activity.to_h[:circuit]
 end
